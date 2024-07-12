41421d93ca35e4c13b34a9bce39feb2d5898a49d
Legimity <lizhaowei0120@foxmail.com>
2024-07-12 20:33:22 +0800

    Added version results for port scanning

--- portScan/port_scan.py
+++ portScan/port_scan.py
@@ -44,60 +44,64 @@ class PortScanner:
         response = analyzer.llama(str(analyze_data))
         return response
 
-
 class nmapScanner():
-  def __init__(self,target_ip,target_port,targe_file=None)->None:
+    def __init__(self, target_ip, target_port, target_file=None) -> None:
         self.target_ip = target_ip
-        self.target_port=target_port
+        self.target_port = target_port
         # 读取配置文件
         config = configparser.ConfigParser(comment_prefixes="#")
         config.read("./conf/conf.ini")
-        self.save_path = config.get("save","save_path")
+        self.save_path = config.get("save", "save_path")
+        self.nm = nmap.PortScanner()
+
+    # 只需要传递ip
+    def run(self) -> str:
+        # 只进行主机发现（ping 扫描）查询主机是否在线
+        self.nm.scan(hosts=self.target_ip, arguments='-sn')
+        host = self.target_ip
+        if host not in self.nm.all_hosts():
+            return {"host": host, "state": "down", "open_ports": []}
         
-  # 只需要传递ip
-  def run(self) -> str:
-      
-      # 只进行主机发现（ping 扫描）查询主机是否在线
-      nm.scan(hosts=self.target_ip, arguments='-sn')
-      host=self.target_ip
-      if host not in nm.all_hosts():
-          return {"host": host, "state": "down", "open_ports": []}
-      
-      # 进行端口扫描
-      nm.scan(hosts=host, arguments='-p 1-65535')
-      
-      host_info = {
-          'host': host,
-          'state': nm[host].state(),
-          'addresses': nm[host].all_protocols(),
-          'open_ports': []
-      }
+        # 进行端口扫描并检测服务版本
+        self.nm.scan(hosts=host, arguments='-p 1-65535 -sV')
+        
+        host_info = {
+            'host': host,
+            'state': self.nm[host].state(),
+            'addresses': self.nm[host].all_protocols(),
+            'open_ports': []
+        }
 
-      for proto in nm[host].all_protocols():
-          lport = nm[host][proto].keys()
-          for port in lport:
-              if nm[host][proto][port]['state'] == 'open':
-                  host_info['open_ports'].append({
-                      'port': port,
-                      'protocol': proto,
-                      'service': nm[host][proto][port]['name']
-                  })
-      host_info = [host_info]
-      with open(self.save_path + "Nmap_" + uuid.uuid1().hex + ".json","w") as result_file:   
-          json.dump(host_info,result_file)
-      return host_info
+        for proto in self.nm[host].all_protocols():
+            lport = self.nm[host][proto].keys()
+            for port in lport:
+                if self.nm[host][proto][port]['state'] == 'open':
+                    service_info = {
+                        'port': port,
+                        'protocol': proto,
+                        'service': self.nm[host][proto][port]['name'],
+                        'version': self.nm[host][proto][port].get('version', 'unknown')
+                    }
+                    host_info['open_ports'].append(service_info)
+        
+        host_info = [host_info]
+        with open(self.save_path + "Nmap_" + uuid.uuid1().hex + ".json", "w") as result_file:
+            json.dump(host_info, result_file, indent=4)
+        return host_info
+    
+      #需要ip和port  
+    def certainPortscan(self):
+            # Your code here to perform the nmap scan
+        nm.scan(self.target_ip, self.target_port, arguments='"-Pn -T4 -sV -sT"')
+        # nm.scan(self.target_ip,arguments="-sP -T4")
+        json_data = nm.analyse_nmap_xml_scan()
+        scan_result = json_data["scan"]
+        # logger.info(f"Scanning the network for a given URL:{ip} and port :{port}.")
+        return scan_result
+        # return "the host is up and port is open"
+
   
-  #需要ip和port  
-  def certainPortscan(self):
-          # Your code here to perform the nmap scan
-      nm.scan(self.target_ip, self.target_port, arguments='"-Pn -T4 -sV -sT"')
-      # nm.scan(self.target_ip,arguments="-sP -T4")
-      json_data = nm.analyse_nmap_xml_scan()
-      scan_result = json_data["scan"]
-      # logger.info(f"Scanning the network for a given URL:{ip} and port :{port}.")
-      return scan_result
-      # return "the host is up and port is open"
-
+  
 
 if __name__ == "__main__":
     nmapscan=nmapScanner(target_ip="127.0.0.1",target_port="8080")
